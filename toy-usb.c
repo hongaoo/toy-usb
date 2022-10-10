80e1da92f4c032f29ef59a2aa4914ad9e9fb089d
hongao <hongao@uniontech.com>
2022-10-10 17:19:51 +0800

    feat: implement toy_usb_probe
    Signed-off-by: hongao <hongao@uniontech.com>

--- toy-usb.c
+++ toy-usb.c
@@ -1,11 +1,56 @@
+
+#include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/usb.h>
+#include <linux/slab.h>
 
-static int toy_usb_probe(struct usb_interface *interface, 
+struct toy_usb_device
+{
+	struct usb_device*		usb_dev;
+	struct usb_interface*	usb_if;
+
+	struct usb_endpoint_descriptor *ep_in;
+	struct usb_endpoint_descriptor *ep_out;
+	struct usb_endpoint_descriptor *ep_intr;
+};
+
+static int toy_usb_probe(struct usb_interface *interface,
 					const struct usb_device_id *id)
 {
-	return 0;
+	struct usb_device* usb_dev = usb_get_dev(interface_to_usbdev(interface));
+	struct usb_endpoint_descriptor *epd;
+	struct usb_host_interface *settings;
+	struct toy_usb_device *toy_usb_dev;
+	int i;
 
+	if(!(toy_usb_dev = kzalloc(sizeof(struct toy_usb_device),GFP_KERNEL)))
+	{
+		dev_dbg(&interface->dev,"could not allocate device memor");		
+		usb_put_dev(toy_usb_dev->usb_dev);
+		return -ENOMEM;
+	}
+
+	//save USB device data
+	toy_usb_dev->usb_dev = usb_dev;
+	toy_usb_dev->usb_if = interface;
+	
+	//find endpoints
+	settings = interface->cur_altsetting;
+	dev_dbg(&interface->dev, "bNumEndpoints=%d", settings->desc.bNumEndpoints);
+
+	for(i = 0; i < settings->desc.bNumEndpoints; i++)
+	{
+       epd = &settings->endpoint[i].desc;                                 
+                                                                          
+       dev_dbg(&interface->dev, "  endpoint=%d type=%d dir=%d addr=%0x", i,
+                usb_endpoint_type(epd), usb_endpoint_dir_in(epd),         
+                usb_endpoint_num(epd));                                   
+                                                                          
+       if (usb_endpoint_is_bulk_in (epd)) toy_usb_dev->ep_in   = epd; else  
+       if (usb_endpoint_is_bulk_out(epd)) toy_usb_dev->ep_out  = epd; else  
+       if (usb_endpoint_xfer_int   (epd)) toy_usb_dev->ep_intr = epd; 	
+	}
+	return 0;
 }
 
 static void toy_usb_disconnect(struct usb_interface *interface)
